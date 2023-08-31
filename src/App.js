4ffef5ac2e72eb22a550287f9f60badcaecd99cd
Rahul singh <100202443+Rahulcse79@users.noreply.github.com>
2023-08-31 23:11:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Navbar1 from './components/Navbar1'
+import Textform1 from './components/Textform1'
+import Alert from './components/Alert';
 
-function App() {
+
+export default function App() {
+
+  const[mode,setmode]=useState('light');
+  const[alert,setAlert]=useState(null);
+  
+  const showAlert=(message,type)=>
+  {
+      setAlert({
+        message: message,
+        type: type
+      })
+
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+  }
+ 
+  const modeclick=()=>
+  {
+    if(mode==='light'){
+    setmode('dark');
+    document.body.style.backgroundColor='#042743';
+    showAlert(" Dark Mode is Enabled","success");
+    document.title='TextConverter-DarkMode';
+    }
+    else{
+    setmode('light');
+    document.body.style.backgroundColor='white';
+    showAlert(" Light Mode is Enabled","success");
+    document.title='TextConverter-LightMode';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+      <Navbar1 h1='TextConverter' mode={mode} modeclick={modeclick}/>
+      <Alert alert={alert}/>
+      <div className="container my-3">
+      <Textform1 showAlert={showAlert} mode={mode}/>
+      </div>
+      <div className="container my-3">
+      </div>
+      </>
+  )
 }
-
-export default App;
